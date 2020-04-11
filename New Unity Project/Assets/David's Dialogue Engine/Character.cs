c3f53ec466141eea93b6c84f7429670c21065c6c
Andrew Crosswait <wer654dnA@gmail.com>
2020-04-11 16:12:08 -0400

    dialogue engine work

---
+++ New Unity Project/Assets/David's Dialogue Engine/Character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using RpgEngine.Dialoug;
+
+
+[Serializable]
+public class Sentence{
+    public string Diologue, Name;
+    public Sprite Frame;
+    [NonSerialized]public float FrameTime;
+
+}
+
+public class Character : MonoBehaviour
+{
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (Talking == false)
+                {
+                    Talking = true;
+                    StartConversation();
+                    print("works");
+                }
+            }
+        }
+
+        else
+        {
+            print("Currently talking");
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Talking = false;
+        End();
+    }
+}
---
+++ New Unity Project/Assets/David's Dialogue Engine/DialogueManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace RpgEngine.Dialoug
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        /// <summary>
+        /// The parent of the dialogue objects.
+        /// </summary>
+        public GameObject dialogueMenu, Options;
+
+        /// <summary>
+        /// The top text box.
+        /// </summary>
+        public Text CharacterATextObject;
+        public Text NameText;
+        
+        /// <summary>
+        /// Set by the person of whome the player has interacted with.
+        /// </summary>
+        public Character talkingTo;
+
+        /// <summary>
+        /// What we are currently displaying.
+        /// </summary>
+        string currentMessage;
+
+        
+        public void DisplayMessage(string chaAMessage, string[] chaBMessage, Character talkingTo)
+        {
+            NameText.text = talkingTo.gameObject.name;
+            timeStartedConversation = Time.time;
+
+            selectedReply = 0;
+
+            currentMessage = chaAMessage;
+
+            replies = chaBMessage;
+
+            this.talkingTo = talkingTo;
+        }
+
+    }
+}
