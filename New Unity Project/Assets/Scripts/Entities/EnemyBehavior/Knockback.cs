3f88453e674f603522f3b8cc33cbfd91126e37a1
Ruel <ruey1@comcast.net>
2020-07-27 14:15:27 -0400

    Adds HitFeedback function to knockback.cs
    HitFeedback makes object flash red for 1 second when called. Used for registering an enemy attack.

--- New Unity Project/Assets/Scripts/Entities/EnemyBehavior/Knockback.cs
+++ New Unity Project/Assets/Scripts/Entities/EnemyBehavior/Knockback.cs
@@ -15,15 +15,25 @@ public class Knockback : MonoBehaviour
             Rigidbody2D Enemy = other.GetComponent<Rigidbody2D>();
             if (Enemy != null)
             {
+                
                 Enemy.isKinematic = false;
                 Vector2 difference = Enemy.transform.position - transform.position;
                 difference = difference.normalized * thrust;
                 Enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(HitFeedback(other));
                 StartCoroutine(KnockCo(Enemy));
                 
 
             }
         }
+    }
+    private IEnumerator HitFeedback(Collider2D collider2D)
+    {
+        // Code to change player character red when hit.     
+
+        collider2D.GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(1f);
+        collider2D.GetComponent<SpriteRenderer>().color = Color.white;
     }
 
     private IEnumerator KnockCo(Rigidbody2D Enemy)
